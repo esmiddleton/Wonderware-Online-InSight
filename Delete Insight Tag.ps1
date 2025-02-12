7e4b1a48e4a550cd61e8d51939b9ead10b8be5c2
esmiddleton <elliott@middletonclan.net>
2025-02-12 11:51:21 -0800

    Add files via upload
    v1.0 -- Script to delete an Insight tag

---
+++ Delete Insight Tag.ps1
@@ -0,0 +1,40 @@
+﻿<# 
+==============================================================
+
+    Delete a tag programmatically from AVEVA Insight
+
+==============================================================
+
+This assumes the source which created the tag has stopped sending tag values--if the source
+is still sending data, the tag will be recreated.
+
+Modified: 12-Feb-2025
+By:       E. Middleton
+
+ To enable Powershell scripts use:
+    Set-ExecutionPolicy unrestricted
+
+ To disable Powershell scripts use (after using the script):
+    Set-ExecutionPolicy restricted
+
+============================================================== 
+#>
+
+# Update this for other regions
+$BaseUrl= 'https://online.wonderware.com/apis/Historian/v2/'
+
+# Login to Insight and open the browser's DevTools (F12) and select the "Network" tab. Copy the "authorization" header from one of the requests
+# and replace the string value below
+$headers = @{
+    Authorization="Bearer eyJhbGciOiJ...."
+}
+
+# Note: You must escape any embedded $ characters in the URL with a backtick `
+$QueryUrl = $BaseUrl + "Tags"
+
+# As written, this deletes a single tag using it's FQN, but you can include a list in the "FQN" array in JSON format
+$toDelete = 'MyDataSource.TheTagToDelete'
+$jsonBody = '{"delete":{"FQN":["' + $toDelete + '"]}}'
+
+$Response = Invoke-WebRequest -Method 'POST' -Headers $headers -Uri $QueryUrl -ContentType "application/json" -Body $jsonBody
+
